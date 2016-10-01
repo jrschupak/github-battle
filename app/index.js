e4c7e3e5d11339a8f5bbf9032e37311dec2a0157
Jonathan Schupak <jrschupak@gmail.com>
2016-10-01 12:24:01 -0400

    added props to HelloWorld comp

--- app/index.js
+++ app/index.js
@@ -3,13 +3,14 @@ var ReactDOM = require ('react-dom');
 
 var HelloWorld = React.createClass({
 	render: function(){
+		console.log(this.props)
 		return(
-			<div>Hello World</div>
+			<div>Hello World I am React</div>
 			)
 	}
 })
 
 ReactDOM.render(
-	<HelloWorld />,
+	<HelloWorld name="Jonathan" anySortOfData={29}/>,
 	document.getElementById('app')
 )
