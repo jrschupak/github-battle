a7e283b78f4d7ee91c32284f86bbfa81b5cd7c4d
Jonathan Schupak <jrschupak@gmail.com>
2016-10-01 20:18:10 -0400

    created IndexRoute

--- app/config/routes.js
+++ app/config/routes.js
@@ -2,14 +2,15 @@ var React = require("react");
 var ReactRouter = require("react-router");
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
-var InderRoute = ReactRouter.IndexRoute;
+var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
 var Main = require('../components/Main');
 var Home = require('../components/Home');
 
 var routes = (
 	<Router>
 		<Route path='/' component={Main}>
-			<Route path='/Home' component={Home} />
+			<IndexRoute component={Home} />
 		</Route>
 	</Router>
 	)
